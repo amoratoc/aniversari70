891be788f8a6c78e2fbde95511fafcba1a93174f
amoratoc <amorato.c@gmail.com>
2025-12-06 18:24:47 +0100

    action to deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// El nombre del repositorio es 'aniversari70'
+const repoName = 'aniversari70'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [
     react(),
     tailwindcss()],
